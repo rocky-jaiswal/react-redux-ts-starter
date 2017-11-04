0eee4dd92dccfa673943a2209942b610366ab423
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2017-11-04 18:58:54 +0100

    Setup basic component.

--- src/containers/Root/index.tsx
+++ src/containers/Root/index.tsx
@@ -5,6 +5,7 @@ import { bindActionCreators } from 'redux';
 import { push } from 'react-router-redux';
 
 import { Dispatch, RootStateType } from '../../constants/types';
+import GameArea from '../../components/GameArea';
 
 interface Props {
   loading: boolean;
@@ -27,10 +28,12 @@ const actions: DispatchProps = {
 const mapDispatchToProps = (dispatch: Dispatch) => bindActionCreators<{}>(actions, dispatch);
 
 export class Root extends React.Component<Props & DispatchProps> {
+
   render() {
     return (
       <div className="container">
         <h1><FormattedMessage id="app.welcome" /></h1>
+        <GameArea width={80} height={80} />
       </div>
     );
   }
