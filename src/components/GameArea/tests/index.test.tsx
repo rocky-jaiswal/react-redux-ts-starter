4231554ed2c627b75073b3f641b910dd5da19c95
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2017-12-06 12:00:33 +0100

    Add enzyme setup.

---
+++ src/components/GameArea/tests/index.test.tsx
@@ -0,0 +1,18 @@
+import { shallow } from 'enzyme';
+import * as React from 'react';
+
+import GameArea from '../';
+
+describe('<GameArea />', () => {
+
+  test('displays without errors', () => {
+    const wrapper = shallow(
+      <GameArea
+        width={100}
+        height={100}
+      />
+    );
+    expect(wrapper.find('.game-area').length).toEqual(1);
+  });
+
+});
